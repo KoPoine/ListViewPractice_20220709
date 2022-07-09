b25a99c84e1f62e2dde3af21ee59058c37174cc6
김민철 <kmc901206@gmail.com>
2022-07-09 13:57:07 +0900

    프로필 액티비티 생성

---
+++ app/src/main/java/com/neppplus/listviewpractice_20220709/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.listviewpractice_20220709
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
