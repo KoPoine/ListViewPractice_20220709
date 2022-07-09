d8479b918653565cd488e07a11e8b4b9bc84c321
김민철 <kmc901206@gmail.com>
2022-07-09 14:19:24 +0900

    리스트뷰 클릭시 Intent를 활용해서 다른 액티비티로 이동 - Intent(2) 활용해서 데이터 첨부

--- app/src/main/java/com/neppplus/listviewpractice_20220709/ProfileActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20220709/ProfileActivity.kt
@@ -2,10 +2,19 @@ package com.neppplus.listviewpractice_20220709
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val name = intent.getStringExtra("name")
+        val birthYear = intent.getIntExtra("birthYear",0)
+        val address = intent.getStringExtra("address")
+
+        nameTxt.text = name
+        ageTxt.text = birthYear.toString()
+        addressTxt.text = address
     }
 }
