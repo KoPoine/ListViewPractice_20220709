af9517a321398e475c44ab8d26f91a2f9992a964
김민철 <kmc901206@gmail.com>
2022-07-09 11:47:15 +0900

    4-2. ArrayAdapter를 상속받는 StudentListAdapter 클래스 생성

--- app/src/main/java/com/neppplus/listviewpractice_20220709/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20220709/adapters/StudentListAdapter.kt
@@ -1,4 +1,16 @@
 package com.neppplus.listviewpractice_20220709.adapters
 
-class StudentListAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.neppplus.listviewpractice_20220709.R
+import com.neppplus.listviewpractice_20220709.StudentData
+
+class StudentListAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<StudentData>
+) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
 }
