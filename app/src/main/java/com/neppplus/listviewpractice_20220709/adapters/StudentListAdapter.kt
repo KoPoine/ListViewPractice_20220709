427b07d1dc6cc84b624b940023bf9a231903778b
김민철 <kmc901206@gmail.com>
2022-07-09 12:20:53 +0900

    4-3. ArrayAdapter가 가지고 있는 getView 함수를 커스터마이징(override) 해서 우리가 만든 한칸의 xml을 연결

--- app/src/main/java/com/neppplus/listviewpractice_20220709/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20220709/adapters/StudentListAdapter.kt
@@ -1,6 +1,7 @@
 package com.neppplus.listviewpractice_20220709.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -8,9 +9,18 @@ import com.neppplus.listviewpractice_20220709.R
 import com.neppplus.listviewpractice_20220709.StudentData
 
 class StudentListAdapter(
-    val mContext : Context,
-    val resId : Int,
-    val mList : ArrayList<StudentData>
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+    //    한칸의 xml에 나타날 모양을 커스터마이징
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
 
+        return row
+    }
 }
