9c66e6e3015da1e1d087b6759c99a1879c96dbb8
김민철 <kmc901206@gmail.com>
2022-07-09 12:39:32 +0900

    5. 어댑터 객체화(리스트 전달) / listView 태그와 연결 - 실데이터 반영x

--- app/src/main/java/com/neppplus/listviewpractice_20220709/MainActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20220709/MainActivity.kt
@@ -2,10 +2,16 @@ package com.neppplus.listviewpractice_20220709
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listviewpractice_20220709.adapters.StudentListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //    ArrayList로 목록 공간을 만들어주고
     val mStudentList = ArrayList<StudentData>()
+
+//    어댑터 클래스 멤버변수화
+    lateinit var mStudentListAdapter : StudentListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +28,10 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(StudentData("김민철8",1990,"서울시 서대문구"))
         mStudentList.add(StudentData("김민철9",1990,"서울시 서대문구"))
         mStudentList.add(StudentData("김민철10",1990,"서울시 서대문구"))
+
+//        실제 어댑터와 ArrayList를 연결(어댑터 클래스를 객체화)
+        mStudentListAdapter = StudentListAdapter(this, R.layout.student_list_item, mStudentList)
+//        액티비티 화면의 listView 태그와 adapter를 연결
+        mainListView.adapter = mStudentListAdapter
     }
 }
